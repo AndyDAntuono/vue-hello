21e296c4e4a2c9ab91c801dd0138e5d797d0d6a3
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-16 17:38:13 +0200

    [UPD] updated both index.html and script.js files

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //all'interno del file script.js imposto una costante la cui funzione (che chiamerò createApp) che avrà il compito di creare il messaggio
-const {createApp} = VTTCue
+const { createApp } = Vue;
 
 //imposto un istanza dell'applicazione Vue sfruttando la funzione createApp
 createApp ({
     data () {
         return {
-            message: 'Hello Wordl By Vue!'
+            message: 'Hello World By Vue!'
         }
     }
-}).mount('#app')
+}).mount('#app');
